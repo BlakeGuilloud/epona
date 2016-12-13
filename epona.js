37e17d8e699fa1d0e5551c7decc09966f216d30a
Blake Guilloud <fbguillo@gmail.com>
2016-12-12 21:40:33 -0500

    Included DELETE

--- epona.js
+++ epona.js
@@ -19,18 +19,6 @@ function post(body) {
   return fetch(request).then(blob => blob.json());
 }
 
-function patch(body) {
-  const request = new Request(body.route, {
-  	method: 'PATCH',
-    mode: body.mode || 'cors',
-    body: JSON.stringify(body.body),
-  	redirect: body.redirect || 'follow',
-  	headers: new Headers(body.headers || defaultHeaders),
-  });
-
-  return fetch(request).then(blob => blob.json());
-}
-
 function put(body) {
   const request = new Request(body.route, {
   	method: 'PUT',
@@ -43,9 +31,33 @@ function put(body) {
   return fetch(request).then(blob => blob.json());
 }
 
+function patch(body) {
+  const request = new Request(body.route, {
+  	method: 'PATCH',
+    mode: body.mode || 'cors',
+    body: JSON.stringify(body.body),
+  	redirect: body.redirect || 'follow',
+  	headers: new Headers(body.headers || defaultHeaders),
+  });
+
+  return fetch(request).then(blob => blob.json());
+}
+
+function del(body) {
+  const request = new Request(body.route, {
+  	method: 'DELETE',
+    mode: body.mode || 'cors',
+  	redirect: body.redirect || 'follow',
+  	headers: new Headers(defaultHeaders),
+  });
+
+  return fetch(request).then(blob => blob.json());
+}
+
 module.exports = {
-  get: get,
+  get: fetch,
   post: post,
   put: put,
   patch: patch,
+  delete: del,
 };
